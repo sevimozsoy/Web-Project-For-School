b7024e5e508046ddfa9448e3e94a675c621708eb
sevimozsoy <74455248+sevimozsoy@users.noreply.github.com>
2021-01-16 19:36:14 +0300

    updates

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,6 +1,6 @@
 ﻿var request = require('postman-request');
 var apiSecenekleri = {
-    sunucu: "http://localhost:3000",
+    sunucu: "https://sevimselinozsoy1811012058.herokuapp.com",
     apiYolu:'/api/mekanlar/'
 }
 
